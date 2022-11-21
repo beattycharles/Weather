a665acd94e799b443e15fa4d2054965f7e26e9f9
beattycharles <113803621+beattycharles@users.noreply.github.com>
2022-11-21 18:38:02 -0500

    still need lon lat

--- assits/script.js
+++ assits/script.js
@@ -11,47 +11,46 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-var apiKey = "28fa50af6fcacf05e7ddaa69610333c7";
+var apiKey = "321bc0050e72840da90a05b26a6fc827";
 var fiveKey = "58f8763bd14af70e49cec94f73742629";
-var input = document.getElementById("Search").value;
+var field = 'Hainseport'; //document.querySelector("#searchInput");
 var buttonA = document.querySelector("#Search");
 var scity = document.querySelector("#scity");
 var cityName = document.querySelector("#cityName");
 var history = [];
-var queryURL = `http://api.openweathermap.org`
-
-
+var queryURL = `https://api.openweathermap.org`;
 function populate() {
-    var { lat } = lat
-    var { lon } = lon
-    var cweather = `${queryURL}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-    
-    fetch(cweather)
+//   var { lat };
+//   var { lon };
+console.log(getcords);
+  var cweather = `${queryURL}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+  fetch(cweather)
     .then((response) => response.json())
     .then((response) => console.log(response))
     .catch((err) => console.error(err));
 }
 
-function getcords(input){
-    var cordApi = `${queryURL}/geo/1.0/direct?q=${input}&limit=5&appid=${apiKey}`;
-    fetch(cordApi)
-    .then(function (res) {
-        console.log(res);
-        return res.json();
-    })
-    // .then(function (data) {
-    //     if (!input[0]) {
-    //         alert('Location not found');
-    //     } else {
-    //         populate();
-    //     }
-    // })
-    // .catch(function (err) {
-    //     console.error(err);
-    // });
+function getcords() {
+    console.log(field);
+  var cordApi = `${queryURL}/geo/1.0/direct?q=${field}&limit=5&appid=${apiKey}`;
+ // var cordApi = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`;
+ fetch(cordApi)
+.then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+      if (!field[0]) {
+          alert('Location not found');
+      } else {
+          populate();
+      }
+  })
+  .catch(function (err) {
+      console.error(err);
+  });
 }
 scity.addEventListener("click", getcords);
-
 
 //localStorage.set("input");
 //for (var i = 0; i < 5; i++)
