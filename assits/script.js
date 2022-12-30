fa452d2d31e1afcb0714bba2abc5a50a231ba982
Seabass <beattycharles@yahoo.com>
2022-12-29 22:48:29 -0500

    created elements for 5day

--- assits/script.js
+++ assits/script.js
@@ -14,6 +14,7 @@
 var apiKey = "321bc0050e72840da90a05b26a6fc827";
 var scity = document.querySelector("#scity");
 var cityName = document.querySelector("#mainCityName");
+var forecastContainer = document.querySelector('#forecast');
 var history = [];
 var queryURL = `https://api.openweathermap.org`;
 dayjs.extend(window.dayjs_plugin_utc);
@@ -24,7 +25,6 @@ function getcords(event) {
   event.preventDefault();
   var field = document.querySelector("#searchInput").value;
   fetch(
-    // `https://api.openweathermap.org/data/2.5/weather?q=${field}&limit=5&appid=${apiKey}`
     "https://api.openweathermap.org/data/2.5/weather?q=" +
       field +
       "&units=imperial&appid=" +
@@ -46,6 +46,7 @@ function getcords(event) {
       cityName = data.name;
       console.log(lat, lon);
       populate();
+      renderItems();
     })
     .catch(function (error) {
       console.log(error);
@@ -65,6 +66,7 @@ function populate() {
     .then((response) => console.log(response))
     .catch((err) => console.error(err));
 showInfo()
+renderForecast()
 }
 
 function showInfo(){
@@ -83,7 +85,96 @@ changeWind.innerHTML = "Wind Speed " + windMph + "mph";
 changeIcon.setAttribute('src', iconUrl);
 changeIcon.setAttribute('alt', iconDescription);
 }
+
+function renderForecastCard(forecast) {
+  // variables for data from api
+  var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+  var iconDescription = forecast.weather[0].description;
+  var tempF = forecast.main.temp;
+  var humidity = forecast.main.humidity;
+  var windMph = forecast.wind.speed;
+
+var col = document.createElement('div');
+var card = document.createElement('div');
+var cardBody = document.createElement('div');
+var cardTitle = document.createElement('h5');
+var weatherIcon = document.createElement('img');
+var tempEl = document.createElement('p');
+var windEl = document.createElement('p');
+var humidityEl = document.createElement('p');
+
+col.append(card);
+card.append(cardBody);
+cardBody.append(cardTitle, weatherIcon, tempEl, windEl, humidityEl);
+
+col.setAttribute('class', 'col-md');
+col.classList.add('five-day-card');
+card.setAttribute('class', 'card bg-primary h-100 text-white');
+cardBody.setAttribute('class', 'card-body p-2');
+cardTitle.setAttribute('class', 'card-title');
+tempEl.setAttribute('class', 'card-text');
+windEl.setAttribute('class', 'card-text');
+humidityEl.setAttribute('class', 'card-text');
+
+  // Add content to elements
+  cardTitle.textContent = dayjs(forecast.dt_txt).format('M/D/YYYY');
+  weatherIcon.setAttribute('src', iconUrl);
+  weatherIcon.setAttribute('alt', iconDescription);
+  tempEl.textContent = `Temp: ${tempF} °F`;
+  windEl.textContent = `Wind: ${windMph} MPH`;
+  humidityEl.textContent = `Humidity: ${humidity} %`;
+
+  forecastContainer.append(col);
+}
+
+function renderForecast(dailyForecast) {
+  // Create unix timestamps for start and end of 5 day forecast
+  var startDt = dayjs().add(1, 'day').startOf('day').unix();
+  var endDt = dayjs().add(6, 'day').startOf('day').unix();
+
+  var headingCol = document.createElement('div');
+  var heading = document.createElement('h4');
+
+  headingCol.setAttribute('class', 'col-12');
+  heading.textContent = '5-Day Forecast:';
+  headingCol.append(heading);
+
+  forecastContainer.innerHTML = '';
+  forecastContainer.append(headingCol);
+
+  for (var i = 0; i < dailyForecast.length; i++) {
+
+    // First filters through all of the data and returns only data that falls between one day after the current data and up to 5 days later.
+    if (dailyForecast[i].dt >= startDt && dailyForecast[i].dt < endDt) {
+
+      // Then filters through the data and returns only data captured at noon for each day.
+      if (dailyForecast[i].dt_txt.slice(11, 13) == "12") {
+        renderForecastCard(dailyForecast[i]);
+      }
+    }
+  }
+}
+
+  function renderItems(data) {
+    renderForecast(data.list);
+  }
+  
+  function fetchWeather(location) {
+    var { lat } = location;
+    var { lon } = location;
+    var city = location.name;
+  
+    var apiUrl = `${queryURL}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+  
+    fetch(apiUrl)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        renderItems(city, data);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
 scity.addEventListener("click", getcords);
-//localStorage.set("input");
-//for (var i = 0; i < 5; i++)
-//append btn for history items
